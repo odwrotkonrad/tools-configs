865bf019efdb4f3e226aef35354fb14ba76d3aa7
konradodwrot <odwrotkonrad@gmail.com>
2026-06-01 09:08:35 +0200

    config(zsh): remove prompt
    Signed-off-by: konradodwrot <odwrotkonrad@gmail.com>

--- root/etc/zsh/rc.d/00-shell.zsh
+++ root/etc/zsh/rc.d/00-shell.zsh
@@ -204,7 +204,7 @@ for opt in ${opts_disabled}; unsetopt ${opt}
 typeset -A zsh_params=(
   HISTFILE    "${XDG_STATE_HOME}/zsh/history"
   HISTSIZE    15_000
-  PS1         '%# '                             #[≟] # for root, % for non root
+  PS1         ''                                #[≟] no prompt at all
   PS2         '> '                              #[≟] prompt when shell waits for input
   PS4         '+ '                              #[≟] prompt when debugging prompt with XTRACE is set
   SAVEHIST    10_000
