1a21c2b1eb3a82f66ec1f7a36433194cbefceeee
konradodwrot <odwrotkonrad@gmail.com>
2026-06-11 13:59:06 +0200

    config(zsh): register zle widgets via wd_fn_rt array loop
    Signed-off-by: konradodwrot odwrotkonrad@gmail.com

--- root-ln/etc/zsh/rc.d/31-zle-widgets.zsh
+++ root-ln/etc/zsh/rc.d/31-zle-widgets.zsh
@@ -1,8 +1,9 @@
-#[…] zshzle widgets
+#[≟] zshzle widgets
 #[⌖] man zshzle > zle
 
-# [≟] abort current input and clear screen in a way it can be scrolled above to see what inputs was aborted
 wd-fn-rt-clear-scrollable() {
+    #[≟] abort current input and clear screen in a way it can be scrolled above to see what inputs was aborted
+
     # 1. print current input as a comment
     if [[ ${BUFFER} ]] {
       print -rn -- "$s_seq[goto-col-1]${(%)PS1}# $BUFFER"
@@ -17,11 +18,11 @@ wd-fn-rt-clear-scrollable() {
     # 4. move empty prompt line to first terminal row
     print -n $s_seq[goto-row-1]
 }
-zle -N wd-fn-rt-clear-scrollable
 
-#[≟] multiline aware accept-line wrapper
-#[∵] native behavior would show PS2 and not use zle multiline editing features otherwise, so multiline editing widgets would not work as expected
 wd-fn-rt-accept-line() {
+    #[≟] multiline aware accept-line wrapper
+    #[∵] native behavior would show PS2 and not use zle multiline editing features otherwise, so multiline editing widgets would not work as expected
+
     local trail=${(M)BUFFER%'\'}
     if [[ $trail ]] {
       LBUFFER+=$'\n'
@@ -30,25 +31,28 @@ wd-fn-rt-accept-line() {
 
     zle .accept-line
 }
-zle -N wd-fn-rt-accept-line
-#[⫶] zshzle widgets
 
-#[…] 🤖🤖🤖 ctrlV [≟] listen for raw key sequences via kitten show-key
-wd-fn-rt-keystrokes-listen() {
-    print
-    kitten show-key -m kitty
-    is-terminal kitty && print -n "${s_seq[csi]}<u"
-    zle reset-prompt
-}
-zle -N wd-fn-rt-keystrokes-listen
-#[⫶] ctrlV
-
-
-#[…] 🤖🤖 ctrlShiftZ [≟] resume the most recent background job (mirror of ^Z suspend)
 wd-fn-rt-job-foreground() {
-    [[ -n ${jobstates} ]] || return
-    [[ -n ${BUFFER} ]] && zle .push-input
-    BUFFER="fg"; zle .accept-line
+    #[≟] resume the most recent background job
+
+    if [[ ! ${jobstates} ]] {
+      return
+    }
+
+    fg
 }
-zle -N wd-fn-rt-job-foreground
-#[⫶] ctrlShiftZ
+
+wd-fn-rt-keystrokes-listen() {
+    #[≟] listen for raw key sequences
+    kitten show-key -m kitty
+}
+
+
+typeset -a wd_fn_rt=(
+   wd-fn-rt-job-foreground
+   wd-fn-rt-accept-line
+   wd-fn-rt-clear-scrollable
+   wd-fn-rt-keystrokes-listen
+)
+
+for widget ( $wd_fn_rt ) zle -N $widget
