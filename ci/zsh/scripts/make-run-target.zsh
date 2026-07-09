51e4df04a0e6559be3ea5988f5407923c3263850
konradodwrot <odwrotkonrad@gmail.com>
2026-07-09 23:30:12 +0200

    ci(make-run-target): rename CHE_RENDER_TEMPLATES_DRY_RUN_SECRETS to CHE_RENDER_TEMPLATES_SKIP_SECRETS
    Export CHE_RENDER_TEMPLATES_SKIP_SECRETS instead of CHE_RENDER_TEMPLATES_DRY_RUN_SECRETS, mapped from MK_DRY_RUN_RENDER_SECRETS environment variable.

--- ci/zsh/scripts/make-run-target.zsh
+++ ci/zsh/scripts/make-run-target.zsh
@@ -9,7 +9,7 @@ typeset root=$(git -C ${0:A:h} rev-parse --show-toplevel)
 export FPATH=$root/ci/zsh/functions:$FPATH
 export PATH=$root/ci/zsh/scripts:$root/ci/zsh/scripts/installs:${GOPATH:-$HOME/go}/bin:/usr/local/go/bin:/usr/local/bin:$PATH
 export CHE_DRY_RUN=$MK_DRY_RUN
-export CHE_RENDER_TEMPLATES_DRY_RUN_SECRETS=$MK_DRY_RUN_RENDER_SECRETS
+export CHE_RENDER_TEMPLATES_SKIP_SECRETS=$MK_DRY_RUN_RENDER_SECRETS
 
 #[why] renders can't call fn-is-virt (templates only see env): computed here, not via $(shell) which this wrapper would annotate
 autoload -Uz fn-is-virt
